f8f8dd10a875ebde88ecf7b073a2ade59d26f1e0
Geferson Rocha <gefersonbrs@gmail.com>
2024-02-23 17:35:47 -0300

    Criando projeto node, instalado express, nodemon e endpoint para listar e buscar estudantes

---
+++ backend/src/database.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        nome: 'Geferson Melo',
+        email: 'teste@gmail.com',
+        cpf: '524.145.655-88',
+        ra: '9454325',
+    },
+
+    {
+        nome: 'Gxfbfvgn Melo',
+        email: 'tesdfge@gmail.com',
+        cpf: '899.145.655-88',
+        ra: '9454585',
+    },
+
+    {
+        nome: 'Geferson Melo',
+        email: 'teste@gmail.com',
+        cpf: '524.145.633-88',
+        ra: '94434355',
+    },
+    
+    {
+        nome: 'ghyfd Melo',
+        email: 'tesggte@gmail.com',
+        cpf: '524.437.655-88',
+        ra: '9453225',
+    }
+]
